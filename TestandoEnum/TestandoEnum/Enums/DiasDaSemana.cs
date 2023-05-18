c1102630975e1bb9c30f582a543c3d1c078f1f85
Paulo Oliveira <paulinho395@gmail.com>
2023-05-18 17:38:21 -0300

    Retornando as praticas

---
+++ TestandoEnum/TestandoEnum/Enums/DiasDaSemana.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestandoEnum.Enums
+{
+    internal enum DiasDaSemana
+    {
+        Segunda,
+        Terça,
+        Quarta
+    }
+}
---
+++ TestandoEnum/TestandoEnum/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using TestandoEnum.Enums;
+
+namespace TestandoEnum // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DiasDaSemana diasDaSemana = new DiasDaSemana();
+            diasDaSemana = DiasDaSemana.Terça;
+
+            Console.WriteLine(diasDaSemana);
+
+            DateTime datas = DateTime.ParseExact("02/03/2025 17:25:22", "dd/MM/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+            Console.WriteLine(datas);
+        }
+    }
+}
