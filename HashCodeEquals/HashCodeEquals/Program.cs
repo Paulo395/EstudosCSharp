62bb6ace33a2aa5667441bedc1026749ed63e359
Paulo Oliveira <paulinho395@gmail.com>
2023-07-24 14:33:04 -0300

    HashSet

---
+++ HashCodeEquals/HashCodeEquals/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Entities;
+
+namespace HashCodeEquals // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client("Maria", "Jão");
+
+            Client b = new Client("Maria", "Jão");
+
+            //Console.WriteLine(a.GetHashCode().Equals(b.GetHashCode())); //Funciona com os dois
+            Console.WriteLine(a.Equals(b));
+
+            //Console.WriteLine(a.GetHashCode());
+            //Console.WriteLine(b.GetHashCode());
+        }
+    }
+}
---
+++ HashSetSortedSet/HashSetSortedSet/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HashSetSortedSet // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<String> set = new HashSet<String>();
+
+            set.Add("a");
+            set.Add("b");
+            set.Add("c");
+            set.Add("d");
+            set.Add("d"); //Não Vai!
+
+            foreach(String s in set)
+            {
+                Console.WriteLine(s);
+            }
+
+            if(set.Contains("j")) Console.WriteLine("Sim");
+
+            Console.WriteLine(set);
+        }
+    }
+}
