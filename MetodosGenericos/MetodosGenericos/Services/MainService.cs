471e4ab5e23ae0fe6d0b39858cd0b843e41a6eac
Paulo Oliveira <paulinho395@gmail.com>
2023-07-28 16:37:00 -0300

    Praticando

---
+++ MetodosGenericos/MetodosGenericos/Services/MainService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal class MainService<T>
+    {
+        HashSet<T> list = new HashSet<T> ();
+        public void AddInfo(T t)
+        {
+            list.Add (t);
+        }
+
+        public void ReturnCollection()
+        {
+            foreach (T t in list)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
